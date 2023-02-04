dbb802258de84b901e82b46e8dc6aa94863f4be2
Rishubh Khurana <rishubhkhurana@gmail.com>
2023-02-04 03:14:56 +0000

    checked in

--- scripts/train.py
+++ scripts/train.py
@@ -1,3 +1,23 @@
 import torch
 import torch.nn as nn
+import hydra
+from omegaconf import DictConfig, OmegaConf
+import pyrootutils
+
+root = pyrootutils.setup_root(
+    search_from=__file__,
+    indicator=[".git", "pyproject.toml"],
+    pythonpath=True,
+    dotenv=True,
+)
+import sys
+sys.path.append(root)
+
+@hydra.main(version_base = '1.2', config_dir = root/'configs', config_name = 'train.yaml')
+def main(cfg: DictConfig):
+    pass
+
+if __name__=='__main__':
+    main()
+
 
--- src/data/load_dataset.py
+++ src/data/load_dataset.py
@@ -1,2 +1,16 @@
 from datasets import load_dataset
+from torch.utils.data import Dataset
+
+
+class HFDataset(Dataset):
+    def __init__(self, name: str = 'fashion_mnist', mode: str = 'train'):
+        super().__init__()
+        self.ds = load_dataset(name)[mode]
+    
+    def __getitem__(self, idx):
+        return self.ds[idx]
+
+    def __len__(self):
+        return len(self.ds)
+
 
